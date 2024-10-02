d6dcd2fe0b901a9bf3e4313cf4dd22ee262c755f
snuffeen <133567875+snuffeen@users.noreply.github.com>
2024-10-02 14:34:58 +0300

    added homepage module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import "./styles.css"
 
+import { homepage } from "./modules/home"
+
+const home = document.querySelector(".home")
+
+home.addEventListener("click", homepage)
 alert('QQQ')
---
+++ src/modules/home.js
@@ -0,0 +1,61 @@
+const container = document.querySelector("#content")
+
+function homepage () {
+    container.textContent = ""
+    addReviewDiv()
+    addScheduleDiv()
+    addLocationDiv()
+}
+
+function addReviewDiv() {
+    const review = document.createElement("div")
+    review.classList.add("review")
+
+    const reviewText = document.createElement("div")
+    reviewText.classList.add("review-text")
+    reviewText.textContent = "Very Nice"
+    
+    const reviewAuthor = document.createElement("div")
+    reviewAuthor.classList.add("reviewAuthor")
+    reviewAuthor.textContent = "John Review"
+    
+    review.appendChild(reviewText)
+    review.appendChild(reviewAuthor)
+    container.appendChild(review)
+}
+
+function addScheduleDiv() {
+    const schedule = document.createElement("div")
+    schedule.classList.add("schedule")
+
+    const scheduleTitle = document.createElement("div")
+    scheduleTitle.classList.add("schedule-title")
+    scheduleTitle.textContent = "Schedule"
+
+    const scheduleHours = document.createElement("div")
+    scheduleHours.classList.add("schedule-hours")
+    scheduleHours.textContent = "Monday: Tuesday: Wednesday: Thursday: Friday: Saturday: Sunday:"
+
+    schedule.appendChild(scheduleTitle)
+    schedule.appendChild(scheduleHours)
+    container.appendChild(schedule)
+}
+
+function addLocationDiv() {
+    const location = document.createElement("div")
+    location.classList.add("location")
+    
+    const locationTitle = document.createElement("div")
+    locationTitle.classList.add("location-title")
+    locationTitle.textContent = "Location"
+
+    const locationAddress = document.createElement("div")
+    locationAddress.classList.add("location-address")
+    locationAddress.textContent = "Ulitsa Pushkina, Dom Kolotushkina"
+
+    location.appendChild(locationTitle)
+    location.appendChild(locationAddress)
+    container.appendChild(location)
+}
+
+export {homepage}
