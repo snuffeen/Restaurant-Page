b281ec12360bb686d4c5afb9e3daad0c7b2eb6cb
snuffeen <133567875+snuffeen@users.noreply.github.com>
2024-10-02 15:18:48 +0300

    added contact page module

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import "./styles.css"
 
 import { homePage } from "./modules/home"
 import { menuPage } from "./modules/menu"
-// import { contactPage } from "./modules/contact"
+import { contactPage } from "./modules/contact"
 
 const home = document.querySelector(".home")
 const menu = document.querySelector(".menu")
-// const contact = document.querySelector(".contact")
+const contact = document.querySelector(".contact")
 
 home.addEventListener("click", homePage)
 menu.addEventListener("click", menuPage)
-// contact.addEventListener("click", contactPage)
+contact.addEventListener("click", contactPage)
--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,3 +1,25 @@
-// export {contactPage}
+export {contactPage}
 
-// const container = document.querySelector("#content")
+const container = document.querySelector("#content")
+
+function contactPage() {
+    container.textContent = ""
+    addContact()
+}
+
+function addContact() {
+    const contact = document.createElement("div")
+    contact.classList.add("contact")
+
+    const contactName = document.createElement("div")
+    contactName.classList.add("contact-name")
+    contactName.textContent = "John Restaurant"
+
+    const contactInfo = document.createElement("div")
+    contactInfo.classList.add("contact-info")
+    contactInfo.textContent = "123-456-789"
+
+    contact.appendChild(contactName)
+    contact.appendChild(contactInfo)
+    container.appendChild(contact)
+}
